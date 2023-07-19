918ab9076c5ba8b7d286ef48ef98f78327f762ec
FoxAhead <foxahead@hotmail.com>
2023-07-20 01:00:37 +0300

    18.6

--- Test1/main.js
+++ Test1/main.js
@@ -1,27 +1,6 @@
 import { RulesTxt } from "./RulesTxt.js";
 import { Civ2 } from "./Civ2.js";
 
-window.addEventListener('unhandledrejection', function (e) {
-  alert("Error occurred: " + e.reason.message);
-})
-window.onerror = function (error, source, lineno, colno, erroro) {
-
-  // Print the error message
-  let output = document.getElementById("errors");
-  output.innerHTML += "Message : " + error + "<br>";
-
-  // Print the url of the file that contains the error
-  output.innerHTML += "Url : " + source + "<br>";
-
-  // Print the line number from which the error generated
-  output.innerHTML += "Line number : " + lineno + "<br>";
-
-  // Print the column number of the error line
-  output.innerHTML += "Column number : " + colno + "<br>";
-
-  // Print he error object
-  output.innerHTML += "Error Object : " + erroro;
-}
 window.onload = main;
 
 const labels1 = [];
@@ -126,21 +105,21 @@ function initVue() {
         this.input.attackingUnit = 1 - this.input.attackingUnit;
       },
       moveUnitsValuesToForm(unitIndex) {
-        // console.log('moveUnitsValuesToForm');
-        const unit = RulesTxt.unitTypes[this.input.unit[unitIndex].type];
-        if (unit != undefined) {
-          this.input.unit[unitIndex].att = unit.att;
-          this.input.unit[unitIndex].def = unit.def;
-          this.input.unit[unitIndex].hit = unit.hit * 10;
-          this.input.unit[unitIndex].firepwr = unit.firepwr;
-        }
+        // // console.log('moveUnitsValuesToForm');
+        // const unit = RulesTxt.unitTypes[this.input.unit[unitIndex].type];
+        // if (unit != undefined) {
+        //   this.input.unit[unitIndex].att = unit.att;
+        //   this.input.unit[unitIndex].def = unit.def;
+        //   this.input.unit[unitIndex].hit = unit.hit * 10;
+        //   this.input.unit[unitIndex].firepwr = unit.firepwr;
+        // }
       },
       checkInput(unitIndex) {
-        if (!RulesTxt.unitTypes[this.input.unit[unitIndex].type].canMakeParadrops && this.input.unit[unitIndex].paradrop) { //Can make paradrops
-          this.$nextTick(() => {
-            this.input.unit[unitIndex].paradrop = false;
-          });
-        }
+        // if (!RulesTxt.unitTypes[this.input.unit[unitIndex].type].canMakeParadrops && this.input.unit[unitIndex].paradrop) { //Can make paradrops
+        //   this.$nextTick(() => {
+        //     this.input.unit[unitIndex].paradrop = false;
+        //   });
+        // }
       },
       callStartCalc() {
         let data1 = {
@@ -211,282 +190,282 @@ function initVue() {
   vm = app.mount('#app')
 }
 
-function initCharts() {
-  myChart1 = new Chart("myChart1", {
-    type: "bar",
-    data: {
-      labels: labels1,
-      datasets: [
-        {
-          backgroundColor: CHART_COLOR[0][0],
-          data: hps1
-        },
-        {
-          backgroundColor: CHART_COLOR[0][1],
-          data: hps1a
-        }
-      ]
-    }
-    ,
-    options: {
-      animation: { duration: 100 },
-      plugins: { legend: false },
-      scales: {
-        // xAxes: [{
-        //   scaleLabel: {
-        //     display: true,
-        //     labelString: 'HP left',
-        //     fontSize: 8
-        //   }
-        // }],
-        yAxes: [{
-          position: 'right',
-          ticks: {
-            beginAtZero: true
-          }
-          // scaleLabel: {
-          //   display: true,
-          //   labelString: '%'
-          // }
-        }]
-      }
-    }
-  });
-  myChart2 = new Chart("myChart2", {
-    type: "bar",
-    data: {
-      labels: labels2,
-      datasets: [
-        {
-          backgroundColor: CHART_COLOR[1][0],
-          data: hps2
-        },
-        {
-          backgroundColor: CHART_COLOR[1][1],
-          data: hps2a
-        }
-      ]
-    }
-    ,
-    options: {
-      animation: { duration: 100 },
-      plugins: { legend: false },
-      scales: {
-        // xAxes: [{
-        //   scaleLabel: {
-        //     display: true,
-        //     labelString: 'HP left'
-        //   }
-        // }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-          //   scaleLabel: {
-          //     display: true,
-          //     labelString: '%'
-          //   }
-        }]
-      }
-    }
-  });
-}
+// function initCharts() {
+//   myChart1 = new Chart("myChart1", {
+//     type: "bar",
+//     data: {
+//       labels: labels1,
+//       datasets: [
+//         {
+//           backgroundColor: CHART_COLOR[0][0],
+//           data: hps1
+//         },
+//         {
+//           backgroundColor: CHART_COLOR[0][1],
+//           data: hps1a
+//         }
+//       ]
+//     }
+//     ,
+//     options: {
+//       animation: { duration: 100 },
+//       plugins: { legend: false },
+//       scales: {
+//         // xAxes: [{
+//         //   scaleLabel: {
+//         //     display: true,
+//         //     labelString: 'HP left',
+//         //     fontSize: 8
+//         //   }
+//         // }],
+//         yAxes: [{
+//           position: 'right',
+//           ticks: {
+//             beginAtZero: true
+//           }
+//           // scaleLabel: {
+//           //   display: true,
+//           //   labelString: '%'
+//           // }
+//         }]
+//       }
+//     }
+//   });
+//   myChart2 = new Chart("myChart2", {
+//     type: "bar",
+//     data: {
+//       labels: labels2,
+//       datasets: [
+//         {
+//           backgroundColor: CHART_COLOR[1][0],
+//           data: hps2
+//         },
+//         {
+//           backgroundColor: CHART_COLOR[1][1],
+//           data: hps2a
+//         }
+//       ]
+//     }
+//     ,
+//     options: {
+//       animation: { duration: 100 },
+//       plugins: { legend: false },
+//       scales: {
+//         // xAxes: [{
+//         //   scaleLabel: {
+//         //     display: true,
+//         //     labelString: 'HP left'
+//         //   }
+//         // }],
+//         yAxes: [{
+//           ticks: {
+//             beginAtZero: true
+//           }
+//           //   scaleLabel: {
+//           //     display: true,
+//           //     labelString: '%'
+//           //   }
+//         }]
+//       }
+//     }
+//   });
+// }
 
-function startCalc(_attacker, _defender) {
-  stopSim();
-  attacker = _attacker;
-  defender = _defender;
-  initCalcArrays();
-  initSimArrays();
-  vm.output.s = 0;
-  removeSimArraysFromChart();
-  undefineObjectProperties(attacker.output.unit);
-  undefineObjectProperties(defender.output.unit);
-  Civ2.setEffectives(
-    attacker.input.unit,
-    defender.input.unit,
-    attacker.output.unit.effective,
-    defender.output.unit.effective,
-    attacker.output.unit.explain,
-    defender.output.unit.explain,
-  );
-  calculate(attacker, defender);
-}
+// function startCalc(_attacker, _defender) {
+//   stopSim();
+//   attacker = _attacker;
+//   defender = _defender;
+//   initCalcArrays();
+//   initSimArrays();
+//   vm.output.s = 0;
+//   removeSimArraysFromChart();
+//   undefineObjectProperties(attacker.output.unit);
+//   undefineObjectProperties(defender.output.unit);
+//   Civ2.setEffectives(
+//     attacker.input.unit,
+//     defender.input.unit,
+//     attacker.output.unit.effective,
+//     defender.output.unit.effective,
+//     attacker.output.unit.explain,
+//     defender.output.unit.explain,
+//   );
+//   calculate(attacker, defender);
+// }
 
-function startSim() {
-  stopSim();
-  addSimArraysToCharts();
-  initSimArrays();
-  vm.output.s = 0;
-  attacker.output.unit.s = 0;
-  defender.output.unit.s = 0;
-  attacker.output.unit.p = 0;
-  defender.output.unit.p = 0;
-  vm.workersCount = 4;
-  for (let i = 0; i < vm.workersCount; i++) {
-    workers.push({
-      w: new Worker("workerSimulateCombats.js", { type: "module" }),
-      finished: false
-    });
-    workers[i].w.onmessage = receiveWorkerMessage;
-  }
-  for (let i = 0; i < workers.length; i++) {
-    workers[i].w.postMessage({
-      unitA: new UnitEffective(attacker.output.unit.effective),
-      unitD: new UnitEffective(defender.output.unit.effective)
-    });
-  }
-}
+// function startSim() {
+//   stopSim();
+//   addSimArraysToCharts();
+//   initSimArrays();
+//   vm.output.s = 0;
+//   attacker.output.unit.s = 0;
+//   defender.output.unit.s = 0;
+//   attacker.output.unit.p = 0;
+//   defender.output.unit.p = 0;
+//   vm.workersCount = 4;
+//   for (let i = 0; i < vm.workersCount; i++) {
+//     workers.push({
+//       w: new Worker("workerSimulateCombats.js", { type: "module" }),
+//       finished: false
+//     });
+//     workers[i].w.onmessage = receiveWorkerMessage;
+//   }
+//   for (let i = 0; i < workers.length; i++) {
+//     workers[i].w.postMessage({
+//       unitA: new UnitEffective(attacker.output.unit.effective),
+//       unitD: new UnitEffective(defender.output.unit.effective)
+//     });
+//   }
+// }
 
-function pa(a, d) {
-  if (d == 0) {
-    return 1 - 1 / a;
-  }
-  else if (a > d) {
-    return 1 - (d + 1) / (2 * a);
-  }
-  else {
-    return (a - 1) / (2 * d);
-  }
-}
+// function pa(a, d) {
+//   if (d == 0) {
+//     return 1 - 1 / a;
+//   }
+//   else if (a > d) {
+//     return 1 - (d + 1) / (2 * a);
+//   }
+//   else {
+//     return (a - 1) / (2 * d);
+//   }
+// }
 
-function calculate(attacker, defender) {
-  if (attacker.input.unit.att > 0) {
-    let effA = attacker.output.unit.effective;
-    let effD = defender.output.unit.effective;
-    let p = pa(effA.att, effD.def);
-    let max = 0;
-    let toWinA = Math.ceil(effD.hit / effA.firepwr);
-    let toWinD = Math.ceil(effA.hit / effD.firepwr);
-    attacker.output.unit.pc = 0;
-    defender.output.unit.pc = 0;
-    for (let k = 0, hp = effA.hit; hp > 0; k++, hp -= effD.firepwr) {
-      let pi = nbd(p, k, toWinA);
-      attacker.output.unit.pc += pi;
-      attacker.output.hps[hp - 1] = (pi * 100).toFixed(2);
-      max = Math.max(max, attacker.output.hps[hp - 1]);
-    }
-    for (let k = 0, hp = effD.hit; hp > 0; k++, hp -= effA.firepwr) {
-      let pi = nbd(1 - p, k, toWinD);
-      defender.output.unit.pc += pi;
-      defender.output.hps[hp - 1] = (pi * 100).toFixed(2);
-      max = Math.max(max, defender.output.hps[hp - 1]);
-    }
-    attacker.output.unit.pc = (attacker.output.unit.pc * 100).toFixed(2);
-    defender.output.unit.pc = (defender.output.unit.pc * 100).toFixed(2);
-    attacker.output.unit.p0 = (p * 100).toFixed(2);
-    defender.output.unit.p0 = (100 - attacker.output.unit.p0).toFixed(2);
-    max = max * 1.05;
-    myChart1.options.scales.yAxes[0].ticks.max = max;
-    myChart2.options.scales.yAxes[0].ticks.max = max;
-  }
-  myChart1.update();
-  myChart2.update();
-}
+// function calculate(attacker, defender) {
+//   if (attacker.input.unit.att > 0) {
+//     let effA = attacker.output.unit.effective;
+//     let effD = defender.output.unit.effective;
+//     let p = pa(effA.att, effD.def);
+//     let max = 0;
+//     let toWinA = Math.ceil(effD.hit / effA.firepwr);
+//     let toWinD = Math.ceil(effA.hit / effD.firepwr);
+//     attacker.output.unit.pc = 0;
+//     defender.output.unit.pc = 0;
+//     for (let k = 0, hp = effA.hit; hp > 0; k++, hp -= effD.firepwr) {
+//       let pi = nbd(p, k, toWinA);
+//       attacker.output.unit.pc += pi;
+//       attacker.output.hps[hp - 1] = (pi * 100).toFixed(2);
+//       max = Math.max(max, attacker.output.hps[hp - 1]);
+//     }
+//     for (let k = 0, hp = effD.hit; hp > 0; k++, hp -= effA.firepwr) {
+//       let pi = nbd(1 - p, k, toWinD);
+//       defender.output.unit.pc += pi;
+//       defender.output.hps[hp - 1] = (pi * 100).toFixed(2);
+//       max = Math.max(max, defender.output.hps[hp - 1]);
+//     }
+//     attacker.output.unit.pc = (attacker.output.unit.pc * 100).toFixed(2);
+//     defender.output.unit.pc = (defender.output.unit.pc * 100).toFixed(2);
+//     attacker.output.unit.p0 = (p * 100).toFixed(2);
+//     defender.output.unit.p0 = (100 - attacker.output.unit.p0).toFixed(2);
+//     max = max * 1.05;
+//     myChart1.options.scales.yAxes[0].ticks.max = max;
+//     myChart2.options.scales.yAxes[0].ticks.max = max;
+//   }
+//   myChart1.update();
+//   myChart2.update();
+// }
 
-/**
- * Negative binomial distribution 
- * @param {float} p - probability of success
- * @param {integer} k - number of failures
- * @param {integer} r - number of successes
- * @returns {float}
- */
-function nbd(p, k, r) {
-  let q = 1 - p;
-  return combinations(k + r - 1, k) * (p ** r) * (q ** k);
-}
+// /**
+//  * Negative binomial distribution
+//  * @param {float} p - probability of success
+//  * @param {integer} k - number of failures
+//  * @param {integer} r - number of successes
+//  * @returns {float}
+//  */
+// function nbd(p, k, r) {
+//   let q = 1 - p;
+//   return combinations(k + r - 1, k) * (p ** r) * (q ** k);
+// }
 
-function productRange(a, b) {
-  var product = a, i = a;
-  while (i++ < b) {
-    product *= i;
-  }
-  return product;
-}
+// function productRange(a, b) {
+//   var product = a, i = a;
+//   while (i++ < b) {
+//     product *= i;
+//   }
+//   return product;
+// }
 
-function combinations(n, k) {
-  if (n == k) {
-    return 1;
-  } else if (k == 0) {
-    return 1;
-  } else {
-    k = Math.max(k, n - k);
-    return productRange(k + 1, n) / productRange(1, n - k);
-  }
-}
+// function combinations(n, k) {
+//   if (n == k) {
+//     return 1;
+//   } else if (k == 0) {
+//     return 1;
+//   } else {
+//     k = Math.max(k, n - k);
+//     return productRange(k + 1, n) / productRange(1, n - k);
+//   }
+// }
 
-function stopSim() {
-  for (let i = 0; i < workers.length; i++) {
-    workers[i].w.terminate();
-    workers[i].w = undefined;
-  }
-  workers.length = 0;
-  vm.workersCount = 0;
-}
+// function stopSim() {
+//   for (let i = 0; i < workers.length; i++) {
+//     workers[i].w.terminate();
+//     workers[i].w = undefined;
+//   }
+//   workers.length = 0;
+//   vm.workersCount = 0;
+// }
 
-function receiveWorkerMessage(event) {
-  if (event.data.finished) {
-    vm.workersCount--;
-  } else {
-    const resultA = event.data.unitA;
-    const resultD = event.data.unitD;
-    for (let i = 0; i < resultA.hps.length; i++) {
-      attacker.output.hpsa[i - 1] += resultA.hps[i] / 400000;
-    }
-    for (let i = 0; i < resultD.hps.length; i++) {
-      defender.output.hpsa[i - 1] += resultD.hps[i] / 400000;
-    }
-    attacker.output.unit.s += resultA.wins;
-    defender.output.unit.s += resultD.wins;
-    vm.output.s = attacker.output.unit.s + defender.output.unit.s;
-    attacker.output.unit.p = (attacker.output.unit.s / vm.output.s * 100).toFixed(2);
-    defender.output.unit.p = (defender.output.unit.s / vm.output.s * 100).toFixed(2);
-    myChart1.update();
-    myChart2.update();
-  }
-}
+// function receiveWorkerMessage(event) {
+//   if (event.data.finished) {
+//     vm.workersCount--;
+//   } else {
+//     const resultA = event.data.unitA;
+//     const resultD = event.data.unitD;
+//     for (let i = 0; i < resultA.hps.length; i++) {
+//       attacker.output.hpsa[i - 1] += resultA.hps[i] / 400000;
+//     }
+//     for (let i = 0; i < resultD.hps.length; i++) {
+//       defender.output.hpsa[i - 1] += resultD.hps[i] / 400000;
+//     }
+//     attacker.output.unit.s += resultA.wins;
+//     defender.output.unit.s += resultD.wins;
+//     vm.output.s = attacker.output.unit.s + defender.output.unit.s;
+//     attacker.output.unit.p = (attacker.output.unit.s / vm.output.s * 100).toFixed(2);
+//     defender.output.unit.p = (defender.output.unit.s / vm.output.s * 100).toFixed(2);
+//     myChart1.update();
+//     myChart2.update();
+//   }
+// }
 
-function initCalcArrays() {
-  initArray(attacker.output.labels, attacker.input.unit.hit, 1, 1);
-  initArray(defender.output.labels, defender.input.unit.hit, 1, 1);
-  initArray(attacker.output.hps, attacker.input.unit.hit);
-  initArray(defender.output.hps, defender.input.unit.hit);
-}
+// function initCalcArrays() {
+//   initArray(attacker.output.labels, attacker.input.unit.hit, 1, 1);
+//   initArray(defender.output.labels, defender.input.unit.hit, 1, 1);
+//   initArray(attacker.output.hps, attacker.input.unit.hit);
+//   initArray(defender.output.hps, defender.input.unit.hit);
+// }
 
-function initSimArrays() {
-  initArray(attacker.output.hpsa, attacker.input.unit.hit);
-  initArray(defender.output.hpsa, defender.input.unit.hit);
-}
+// function initSimArrays() {
+//   initArray(attacker.output.hpsa, attacker.input.unit.hit);
+//   initArray(defender.output.hpsa, defender.input.unit.hit);
+// }
 
-function addSimArraysToCharts() {
-  myChart1.data.datasets[1] = {
-    backgroundColor: CHART_COLOR[0][1],
-    data: hps1a
-  }
-  myChart2.data.datasets[1] = {
-    backgroundColor: CHART_COLOR[1][1],
-    data: hps2a
-  }
-  myChart1.update();
-  myChart2.update();
-}
+// function addSimArraysToCharts() {
+//   myChart1.data.datasets[1] = {
+//     backgroundColor: CHART_COLOR[0][1],
+//     data: hps1a
+//   }
+//   myChart2.data.datasets[1] = {
+//     backgroundColor: CHART_COLOR[1][1],
+//     data: hps2a
+//   }
+//   myChart1.update();
+//   myChart2.update();
+// }
 
-function removeSimArraysFromChart() {
-  myChart1.data.datasets.length = 1;
-  myChart2.data.datasets.length = 1;
-  myChart1.update();
-  myChart2.update();
-}
+// function removeSimArraysFromChart() {
+//   myChart1.data.datasets.length = 1;
+//   myChart2.data.datasets.length = 1;
+//   myChart1.update();
+//   myChart2.update();
+// }
 
-function initArray(arr, len, first = 0, delta = 0) {
-  arr.length = len;
-  arr.fill(0);
-  if (delta > 0) {
-    for (let i = 0, j = first; i < arr.length; i++, j += delta) {
-      arr[i] = j;
-    }
-  }
-}
+// function initArray(arr, len, first = 0, delta = 0) {
+//   arr.length = len;
+//   arr.fill(0);
+//   if (delta > 0) {
+//     for (let i = 0, j = first; i < arr.length; i++, j += delta) {
+//       arr[i] = j;
+//     }
+//   }
+// }
 
 // function undefineObjectProperties(obj) {
 //   if (obj instanceof Array) {
